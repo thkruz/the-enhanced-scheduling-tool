8324063413b67722c07e84042453b79586f7fa57
Theodore Kruczek <theodore.kruczek@gmail.com>
2022-02-16 13:02:34 -0500

    feat: :sparkles: starting point for backend server
    #9

--- server/index.js
+++ server/index.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const cors = require('cors');
+const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
+// middleware
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.listen(port, () => {
+  console.log(`Scheduling App listening on port ${port}`);
+});
+
+app.get('/', cors(), (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.use('/calendar', cors(), express.static('./calendar.json'));
+
+app.post('/api', cors(), async (req, res) => {
+  const { api } = req.body;
+  if (api) {
+    res.send(JSON.stringify(api));
+  } else {
+    res.end(JSON.stringify({ error: 'Invalid API Call!' }));
+  }
+});
+
+app.get('/user/:userid', cors(), (req, res) => {
+  const { userid } = req.params;
+  if (userid) {
+    res.send(JSON.stringify(userid));
+  } else {
+    res.end(JSON.stringify({ error: 'Invalid User ID!' }));
+  }
 });
