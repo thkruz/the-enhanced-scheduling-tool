fb85fc109a6074c5860aad2a89356662b495f1ba
Collin Gilmore <collin.gilmore.1@us.af.mil>
2022-02-16 17:01:22 -0500

    feat: start memberDay framework

---
+++ frontend/src/components/Header/MemberDay/MemberDay.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import MemberDay from './MemberDay';
+
+
+let memberObj =
+
+describe('MemberDay', () => {
+
+    it('It should display the scheduling app title', () => {
+        render (
+            <MemberDay member={memberObj}/>
+        );
+        expect(screen.getByRole('heading', {  name: /Scheduling App/i})).toBeInTheDocument();
+    });
+});
