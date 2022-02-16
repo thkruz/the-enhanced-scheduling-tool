c7e2f97776cd41d28f7b2246a9d9ccdbaea8275e
Collin Gilmore <collin.gilmore.1@us.af.mil>
2022-02-16 18:02:27 -0500

    enhancement: end of day - need to finish member Test
    Co-authored-by: Theodore Kruczek <theodore.kruczek@gmail.com>

--- frontend/src/components/Header/MemberDay/MemberDay.test.jsx
+++ frontend/src/components/Header/MemberDay/MemberDay.test.jsx
@@ -5,11 +5,29 @@ import MemberDay from './MemberDay';
 let memberObj =
 
 describe('MemberDay', () => {
-
-    it('It should display the scheduling app title', () => {
+    beforeEach( () => {
         render (
-            <MemberDay member={memberObj}/>
+            <MemberDay currentDate={new Date(2022,1,1)} member={memberObj}/>
         );
+    });
+    it('should show 3 shifts per day', () => {
+
+        expect(screen.getAllByRole('listitem', {  name: /day/i})).toBeInTheDocument();
+        expect(screen.getAllByRole('listitem', {  name: /swing/i})).toBeInTheDocument();
+        expect(screen.getAllByRole('listitem', {  name: /mid/i})).toBeInTheDocument();
+    });
+
+    it('should fill in entry with red background if unavailable', () => {
+        const memberDayCard = screen.getByTestId('member-day-card');
+        
+        expect(screen.getByRole('heading', {  name: /Scheduling App/i})).toBeInTheDocument();
+    });
+
+    it('should update memberObj to unavailable if clicked', () => {
+
+    });
+
+    it('should update a the member state with their availability that day', () => {
         expect(screen.getByRole('heading', {  name: /Scheduling App/i})).toBeInTheDocument();
     });
 });
