15f03de18834a1d1dd2c5e6be4e0c839e6976662
Collin Gilmore <collin.gilmore.1@us.af.mil>
2022-02-16 16:36:11 -0500

    fix: fix CI error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Routes, Route, useLocation} from 'react-router-dom';
+import {Routes, Route, useLocation} from 'react-router-dom';
 import Header from './components/Header/Header';
 
 function App() {
