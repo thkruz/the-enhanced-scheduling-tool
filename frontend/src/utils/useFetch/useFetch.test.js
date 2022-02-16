b4da39272e04d196c8595fe10619f4ae9c406a2f
Theodore Kruczek <theodore.kruczek@gmail.com>
2022-02-16 17:28:22 -0500

    style: :art: reformatted files using prettier

--- frontend/src/utils/useFetch/useFetch.test.js
+++ frontend/src/utils/useFetch/useFetch.test.js
@@ -1,20 +1,18 @@
-import { getByRole, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import FakeComponent from './FakeComponent';
 
-
 describe('Unit tests for useFetch custom hook', () => {
-
-  const setup = (url) => {
-    render(<FakeComponent url={url} />)
-  }
+  const setup = url => {
+    render(<FakeComponent url={url} />);
+  };
 
   test('the test itself should render a FakeComponent to use for further testing', () => {
     // Decide what fetch is going to send back
-    jest.spyOn(window, "fetch").mockImplementation(() => {
+    jest.spyOn(window, 'fetch').mockImplementation(() => {
       const fetchResponse = {
         ok: true,
-        json: () => Promise.resolve(JSON.stringify({id: 0, name: "John Doe"}))
+        json: () => Promise.resolve(JSON.stringify({ id: 0, name: 'John Doe' })),
       };
       return Promise.resolve(fetchResponse);
     });
@@ -23,12 +21,11 @@ describe('Unit tests for useFetch custom hook', () => {
     const btn = screen.getByRole('button');
     expect(btn).toBeInTheDocument();
     userEvent.click(btn);
-  })
+  });
 
   // test('the test should return null data, null err, and true load', () => {
   //   setup('');
   //   const dataText = screen.getByText('/data:/i');
   //   expect(dataText).toBeInTheDocument();
   // })
-
-})
+});
