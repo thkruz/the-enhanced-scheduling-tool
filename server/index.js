d25e4103886f1a1904cb6515fc906f2a50c438c7
Theodore Kruczek <theodore.kruczek@gmail.com>
2022-02-16 13:44:17 -0500

    fix: :bug: enabled cors on server from frontend on localhost:3000

--- server/index.js
+++ server/index.js
@@ -6,6 +6,17 @@ const port = process.env.PORT || 3001;
 // middleware
 app.use(express.json());
 app.use(express.urlencoded());
+app.use((req, res, next) => {
+  res.header({ 'Access-Control-Allow-Origin': 'http://localhost:3000' });
+  res.header({
+    'Access-Control-Allow-Headers':
+      'Origin, X-Requested-With, Content-Type, Accept',
+  });
+  res.header({
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+  });
+  next();
+});
 
 app.listen(port, () => {
   console.log(`Scheduling App listening on port ${port}`);
