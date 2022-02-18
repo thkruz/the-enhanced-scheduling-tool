8236a969478e284fcc470859c73ed38ffceef1ec
Tony Kelly <patient-remix-0q@icloud.com>
2022-02-18 11:50:46 -0500

    style: add some basic style to main App for mapping general layout from Wireframes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,58 @@
-//import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Loading from './components/Loading';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 0.3rem;
+  border: 1px solid black;
+`
+const Main = styled.main`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  wrap: no-wrap;
+  width: 100%;
+`
+const Left = styled.section`
+  display: flex;
+  width: 300px;
+  justify-content: flex-start;
+  border: 1px solid black;
+`
+
+const Center = styled.section`
+  display: flex;
+  justify-content: center;
+  border: 1px solid black;
+  width: calc(100% - 300px - 35px);
+`
+
+const Right = styled.section`
+  display: flex;
+  width: 35px;
+  justify-content: left;
+  border: 1px solid black;
+`
 
 function App() {
   return (
-    <>
+    <Container>
       <Header location={useLocation().pathname} />
-      <Routes>
-        <Route path="/" element={<></>} />
-      </Routes>
-    </>
+        <Main>
+          <Left>Left Content</Left>
+          <Center>
+            <Routes>
+              <Route path="/" element={<Loading />} />
+            </Routes>
+          </Center>
+          <Right>PIX</Right>
+        </Main>
+    </Container>
   );
 }
 
