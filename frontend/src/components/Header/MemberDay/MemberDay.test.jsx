aadb828ad6f35c7100a300ccb36bcb317d9609c7
Tony Kelly <patient-remix-0q@icloud.com>
2022-02-18 15:23:45 -0500

    test: needed to comment out unfinished code in MemberDay test

--- frontend/src/components/Header/MemberDay/MemberDay.test.jsx
+++ frontend/src/components/Header/MemberDay/MemberDay.test.jsx
@@ -2,13 +2,14 @@ import { render, screen } from '@testing-library/react';
 import MemberDay from './MemberDay';
 
 
-let memberObj =
+// let memberObj =
 
 describe('MemberDay', () => {
 
     it('It should display the scheduling app title', () => {
         render (
-            <MemberDay member={memberObj}/>
+            // <MemberDay member={memberObj}/>
+            <MemberDay />
         );
         expect(screen.getByRole('heading', {  name: /Scheduling App/i})).toBeInTheDocument();
     });
