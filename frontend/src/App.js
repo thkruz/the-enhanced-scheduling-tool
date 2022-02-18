2c8fa852de4b6715abccd926157883d913aa8467
Tony Kelly <patient-remix-0q@icloud.com>
2022-02-18 12:06:47 -0500

    style: removed dark borders, fixed shading
    adds a basic scaffolding for app layout for us to work from. It can be easily changed as people want to revamp it.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,9 +8,19 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin: 0 0.3rem;
-  border: 1px solid black;
+  margin: 0 0.5rem;
+  border-radius: 10px;
+  box-shadow: 3px 4px 4px rgba(0, 0, 0, 0.25);
+  background: #F5F9FB;
 `
+
+const Navigation = styled(Container)`
+  width: 100%;
+  border-radius: 0;
+  box-shadow: none;
+  margin-bottom: 1rem;
+`
+
 const Main = styled.main`
   display: flex;
   flex-direction: row;
@@ -22,27 +32,28 @@ const Left = styled.section`
   display: flex;
   width: 300px;
   justify-content: flex-start;
-  border: 1px solid black;
 `
 
 const Center = styled.section`
   display: flex;
   justify-content: center;
-  border: 1px solid black;
   width: calc(100% - 300px - 35px);
+  border-left: 1px solid #cbd6db;
+  border-right: 1px solid #cbd6db;
 `
 
 const Right = styled.section`
   display: flex;
   width: 35px;
-  justify-content: left;
-  border: 1px solid black;
+  justify-content: center;
 `
 
 function App() {
   return (
     <Container>
-      <Header location={useLocation().pathname} />
+      <Navigation>
+        <Header location={useLocation().pathname} />
+      </Navigation>
         <Main>
           <Left>Left Content</Left>
           <Center>
