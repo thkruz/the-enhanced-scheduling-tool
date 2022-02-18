0a50f9921ccea5935aa72328e141f5502f5ab4a9
Tony Kelly <patient-remix-0q@icloud.com>
2022-02-18 10:54:41 -0500

    feat: add Loading component scaffolding

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,15 +1,10 @@
-import { screen, render } from '@testing-library/react';
-import Loading from './Loading';
+import React from 'react';
 
-describe('unit tests for the Loading component', () => {
+export default function Loading() {
 
-  const setup = () => {
-    render(<Loading />);
-  }
-
-  test('it should display a header with the words Loading onto the page', () => {
-    setup();
-    const header = screen.findByRole('header', '/loading/i');
-    expect(header).toBeInTheDocument();
-  })
-})
+  return (
+    <div>
+      null
+    </div>
+  )
+}
