33be2aee62931769c3c832a0b2675d08afc53a35
Theodore Kruczek <theodore.kruczek@gmail.com>
2022-02-19 16:27:38 -0500

    style: :rotating_light: fix eslint issue with duplicate prefix

--- frontend/src/components/Header/MemberDay/MemberDay.test.jsx
+++ frontend/src/components/Header/MemberDay/MemberDay.test.jsx
@@ -1,16 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import MemberDay from './MemberDay';
 
-
 // let memberObj =
 
 describe('MemberDay', () => {
-
-    it('It should display the scheduling app title', () => {
-        render (
-            // <MemberDay member={memberObj}/>
-            <MemberDay />
-        );
-        expect(screen.getByRole('heading', {  name: /Scheduling App/i})).toBeInTheDocument();
-    });
+  it('should display the scheduling app title', () => {
+    render(
+      // <MemberDay member={memberObj}/>
+      <MemberDay />
+    );
+    expect(screen.getByRole('heading', { name: /Scheduling App/i })).toBeInTheDocument();
+  });
 });
