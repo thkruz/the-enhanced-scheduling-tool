4213c79bccda4a8ec73c2f2ed2837c3d483f5d37
Tony Kelly <patient-remix-0q@icloud.com>
2022-02-20 21:18:05 -0500

    chore: fixes dev dependencies

--- frontend/src/pages/About/Calendar.jsx
+++ frontend/src/pages/About/Calendar.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
 import Loading from "../../components/Loading/Loading";
 
 const Calendar = () => {
 
+  
   return (
     <>
       <Loading />
