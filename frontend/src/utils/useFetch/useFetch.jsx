5b2414167a1d4001f1abf7d08bbdef2bab87f241
Tony Kelly <patient-remix-0q@icloud.com>
2022-02-20 21:53:54 -0500

    feat: upgraded useFetch for POST for use on routes

--- frontend/src/utils/useFetch/useFetch.jsx
+++ frontend/src/utils/useFetch/useFetch.jsx
@@ -2,13 +2,14 @@ import { useState, useEffect } from 'react';
 
 const baseUrl = 'http://localhost:3001/';
 
-const useFetch = urlRoute => {
+const useFetch = (urlRoute, method, options) => {
   const [data, setData] = useState([]);
   const [err, setErr] = useState(null);
   const [load, setLoad] = useState(true);
 
   useEffect(() => {
-    fetch(baseUrl + urlRoute)
+    if (method === 'GET') {
+      fetch(baseUrl + urlRoute)
       .then(response => {
         if (response.ok) {
           return response.json();
@@ -19,6 +20,24 @@ const useFetch = urlRoute => {
       .then(json => setData(json))
       .catch(e => setErr(e))
       .finally(() => setLoad(false));
+    }
+    else if (method === 'POST') {
+      fetch(baseUrl + urlRoute)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Cannot convert response to json');
+        }
+      })
+      .then(json => setData(json))
+      .catch(e => setErr(e))
+      .finally(() => setLoad(false));
+    } else {
+      setData([]);
+      setErr(new Error(`fetch by ${method} was unsucessful`));
+      setLoad(true);
+    }
   }, [urlRoute]);
 
   return { data, err, load };
