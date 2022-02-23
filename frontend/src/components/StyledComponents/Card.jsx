78d125808692d0ae0ef746a357aaeb5e1366d673
Tony Kelly <patient-remix-0q@icloud.com>
2022-02-23 15:52:16 -0500

    feat: :sparkles: about page pulls from github api

---
+++ frontend/src/components/StyledComponents/Card.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Card = styled.div`
+display: flex;
+flex-direction: column;
+margin: 0 0.5rem;
+border-radius: 10px;
+box-shadow: 3px 4px 4px rgba(0, 0, 0, 0.25);
+background-color: black;
+width: 300px;
+padding: 1rem;
+margin: 1rem;
+`
---
+++ frontend/src/components/StyledComponents/List.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const List = styled.article`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`
