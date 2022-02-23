445c7518dde03472a7a13b26c6b07333a60569e1
Tony Kelly <patient-remix-0q@icloud.com>
2022-02-23 14:27:15 -0500

    refactor: :recycle: uses new route to fetch id directly

--- frontend/src/pages/UsersDetails/UsersDetails.jsx
+++ frontend/src/pages/UsersDetails/UsersDetails.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 
 import Loading from '../../components/Loading/Loading';
@@ -7,20 +7,23 @@ import useFetch from '../../utils/useFetch/useFetch';
 
 const UserDetails = () => {
 
-  const [member, setMember] = useState({});
   const params = useParams();
-  const {data, err, load} = useFetch('roster');
+  const { data, err, load } = useFetch(`user/${params.id}`);
 
   useEffect( () => {
-    setMember(data.filter( entry => entry.id === Number(params.id))[0])
   }, [data, err, load]);
 
   if (load) return <Loading />
 
   return (
     <div>
-      <p>{member.first}</p>
-      <p>{member.last}</p>>
+      <p>{data.first}</p>
+      <p>{data.last}</p>
+      <select defaultValue={data.preference}>
+        <option>Day</option>
+        <option>Swing</option>
+        <option>Mid</option>
+      </select>
     </div>
   )
 
