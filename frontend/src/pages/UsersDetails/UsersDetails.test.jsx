2b84316183d5557ad08f694ce910d1f7c3a0cfc5
Tony Kelly <patient-remix-0q@icloud.com>
2022-02-23 12:49:33 -0500

    test: :test_tube: checked for loading and first name
    checks that the Loading components shows then async awaits for John (user 1) to appear

---
+++ frontend/src/pages/UsersDetails/UsersDetails.test.jsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
+import UsersDetails from './UsersDetails';
+
+import roster from '../../../../server/roster.json'
+
+describe('testing the UserDetals on the /users/:id route', () => {
+
+  const setup = () => {
+    render (
+      <MemoryRouter initialEntries={['/users/1']} initialIndex={1} >
+        <Routes>
+          <Route path='/users/:id' element={<UsersDetails />} />
+        </Routes>
+      </MemoryRouter>
+    );
+  }
+
+  it('should display the first name of the user', async () => {
+    setup();
+    expect(screen.getByRole('heading', {name: /loading/i})).toBeInTheDocument();
+    expect( await screen.findByText('John')).toBeInTheDocument();
+  });
+
+});
