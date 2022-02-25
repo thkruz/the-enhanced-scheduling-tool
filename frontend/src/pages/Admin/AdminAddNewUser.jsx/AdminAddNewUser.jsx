f76553ef705c9bb28524a58f28ed32422befb814
tkbdsi <patient-remix-0q@icloud.com>
2022-02-25 16:14:50 -0600

    feat: :sparkles: added cap to add new user to select shift preference

--- frontend/src/pages/Admin/AdminAddNewUser.jsx/AdminAddNewUser.jsx
+++ frontend/src/pages/Admin/AdminAddNewUser.jsx/AdminAddNewUser.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { RuxInput } from '../../../../node_modules/@astrouxds/react/dist/components';
+import { RuxInput, RuxRadioGroup, RuxRadio } from '../../../../node_modules/@astrouxds/react/dist/components';
 const AdminAddNewUser = () => {
 
     return (
         <form>
             <RuxInput type="text" placeholder="Enter First Name"/>
             <RuxInput type="text" placeholder="Enter Last Name"/>
+            <RuxRadioGroup data-testid="data-testid-radio">
+                <RuxRadio>Day</RuxRadio>
+                <RuxRadio>Swing</RuxRadio>
+                <RuxRadio>Mid</RuxRadio>
+            </RuxRadioGroup>
         </form>
     )
 }
