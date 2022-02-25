94dd5adf9df25b16d47600b96ff25b740e3b2522
tkbdsi <patient-remix-0q@icloud.com>
2022-02-24 22:54:46 -0600

    feat: :sparkles: passes test for having Remove User button

---
+++ frontend/src/pages/Admin/Admin.jsx
@@ -0,0 +1,18 @@
+import React, {useContext} from 'react';
+import { SchedulerContext } from '../../SchedulerContext';
+import { RuxButton } from '../../../node_modules/@astrouxds/react/dist/components';
+
+const Admin = () => {
+
+    const scheduler = useContext(SchedulerContext);
+
+    console.log(scheduler);
+
+    return (
+        <div style={{width: "90%", padding: "1rem"}}>
+            <RuxButton data-testid="data-testid-ru">Remove User</RuxButton>
+        </div>
+    )
+}
+
+export default Admin;
