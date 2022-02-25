2642444d8483bc025cab8c586cf65f78b6570ae4
tkbdsi <patient-remix-0q@icloud.com>
2022-02-25 08:18:33 -0600

    style: :lipstick: changed to flex-grow and added scrollbar to center

--- frontend/src/components/StyledComponents/Center.jsx
+++ frontend/src/components/StyledComponents/Center.jsx
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 export const Center = styled.section`
   display: flex;
   justify-content: center;
-  width: calc(100% - 200px - 80px);
+  flex-grow: 1;
+  overflow-y: auto;
   background: var(--color-global-tertiary-700);
   margin: 0 1rem;
 `;
