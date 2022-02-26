d0c8e885a0f178e1a2a5d0b1a585a88e59334889
tkbdsi <patient-remix-0q@icloud.com>
2022-02-25 21:22:08 -0600

    docs: :memo: added documentation by buttons to communicate with Ted and Collin

--- frontend/src/pages/Admin/Admin.jsx
+++ frontend/src/pages/Admin/Admin.jsx
@@ -37,16 +37,16 @@ const Admin = () => {
                     status === '' && 
                     <div>
                         <Padding>
-                            <RuxButton data-testid="data-testid-ud" onClick={handleDataUpload}>Upload Data</RuxButton>
+                            <RuxButton data-testid="data-testid-ud" onClick={handleDataUpload}>Upload Data</RuxButton><span>TBD, only alerts</span>
                         </Padding>
                         <Padding>
-                            <RuxButton data-testid="data-testid-ed" onClick={handleExportData}>Export Data</RuxButton>
+                            <RuxButton data-testid="data-testid-ed" onClick={handleExportData}>Export Data</RuxButton><span>TBD, only alerts</span>
                         </Padding>
                         <Padding>
-                            <RuxButton data-testid="data-testid-au" onClick={handleAddNewUser}>Add New User</RuxButton>
+                            <RuxButton data-testid="data-testid-au" onClick={handleAddNewUser}>Add New User</RuxButton><span>Completed</span>
                         </Padding>
                         <Padding>
-                            <RuxButton data-testid="data-testid-ru" onClick={handleRemoveUser}>Remove User</RuxButton>
+                            <RuxButton data-testid="data-testid-ru" onClick={handleRemoveUser}>Remove User</RuxButton><span>TBD, only alerts</span>
                         </Padding>
                     </div>
                 }
