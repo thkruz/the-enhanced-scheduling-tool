7b955c22a15774c01b21e354379cd80476e59a11
Tony Kelly <patient-remix-0q@icloud.com>
2022-02-28 10:04:36 -0500

    test: cypress test checks for correct title on load

--- cypress/integration/fulle2e.spec.js
+++ cypress/integration/fulle2e.spec.js
@@ -2,6 +2,11 @@
 
 describe('End-to-End Tests for Enhanced Scheduling App', () => {
   
+  it('renders the title Scheduline App upon load', () => {
+    cy.visit('/');
+    cy.get('#title').should('have.text', "Scheduling App");
+  })
+
   it('renders the Quick Navigation list on load', () => {
     cy.visit('/');
     cy.get('#quickNavigationList').find('li').should('have.length', 3);
