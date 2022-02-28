aaa5edeee3251bba75a3b3425ea93427fbe87a6f
Tony Kelly <patient-remix-0q@icloud.com>
2022-02-28 10:01:47 -0500

    test: cypress test checks for at least 7 days on schedule

--- cypress/integration/fulle2e.spec.js
+++ cypress/integration/fulle2e.spec.js
@@ -7,14 +7,20 @@ describe('End-to-End Tests for Enhanced Scheduling App', () => {
     cy.get('#quickNavigationList').find('li').should('have.length', 3);
   });
 
-  it('renders the roster of members upon loading ', () => {
+  it('renders the roster of members on load ', () => {
     cy.visit('/');
     cy.get('#rosterList').find('li').should('have.length.gte', 1);
   })
 
-  it('renders a Download button upon loading', () => {
+  it('renders a Download button on load', () => {
     cy.visit('/');
     cy.get('#downloadButton').should('have.text','Download');
   })
+
+  it('renders at least 7 schedule days on load', () => {
+    cy.visit('/');
+    cy.get('.shiftCard').should('have.length.gte', 7);
+  })
+
 
 })
