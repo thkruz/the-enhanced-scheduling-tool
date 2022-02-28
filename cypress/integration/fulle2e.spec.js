2acac0152a57aa8112821ae0fa3da3adc7fca960
Tony Kelly <patient-remix-0q@icloud.com>
2022-02-28 09:26:21 -0500

    test: :test_tube: fixed test to visit the default route

--- cypress/integration/fulle2e.spec.js
+++ cypress/integration/fulle2e.spec.js
@@ -3,6 +3,7 @@
 describe('End-to-End Tests for Enhanced Scheduling App', () => {
   
   it('renders the Quick Navigation list on load', () => {
+    cy.visit('/');
     cy.get('#quickNavigationList').find('li').should('have.length', 3);
   });
 
