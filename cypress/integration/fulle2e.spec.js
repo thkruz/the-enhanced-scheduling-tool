a0864abd35694407140a944567d8c13bbac7b353
Tony Kelly <patient-remix-0q@icloud.com>
2022-02-28 09:29:39 -0500

    test: added cypress test to show roster of members

--- cypress/integration/fulle2e.spec.js
+++ cypress/integration/fulle2e.spec.js
@@ -7,4 +7,9 @@ describe('End-to-End Tests for Enhanced Scheduling App', () => {
     cy.get('#quickNavigationList').find('li').should('have.length', 3);
   });
 
+  it('renders the roster of members upon loading ', () => {
+    cy.visit('/');
+    cy.get('#rosterList').find('li').should('have.length.gte', 1);
+  })
+  
 })
