2e5cbed98b77f463aab71bb0764fafe9c43fd2f4
Tony Kelly <patient-remix-0q@icloud.com>
2022-02-28 10:38:05 -0500

    test: difficult test refactor complete (see details)
    The RuxGlobalStatusBar made it very difficult to find the title. This is due to the UI package combining lots of features in the background. This test now finds the h1 embedded deep within the component to verify the title change.

--- cypress/integration/fulle2e.spec.js
+++ cypress/integration/fulle2e.spec.js
@@ -2,11 +2,12 @@
 
 describe('End-to-End Tests for Enhanced Scheduling App', () => {
   
-  it('renders the title Scheduline App upon load', () => {
+  it('renders the title Scheduling App upon load', () => {
     cy.visit('/');
-    cy.get('#title').should('have.text', "Scheduling App");
+    cy.get('rux-global-status-bar').shadow().find('h1').should('have.text','TESTSCHEDULING APP');
   })
 
+  
   it('renders the Quick Navigation list on load', () => {
     cy.visit('/');
     cy.get('#quickNavigationList').find('li').should('have.length', 3);
